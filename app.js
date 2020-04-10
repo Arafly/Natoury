6f7f15bc488e4767cd38688ad3548bfdaceb8f45
Ayinde Ayobami <ayinde.ayobami@venturegardengroup.com>
2020-04-10 21:33:51 +0100

    Response to URL params

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 // 
 const ora = require('ora');
 // const throbber = ora('Shimmy shimmy yay, shimmy yay, shimmy ya. Swalla-la-la').start();
@@ -35,7 +36,42 @@ app.get("/api/v1/tours", (req, res)=> {
     })
 })
 
+app.get("/api/v1/tours/:id", (req, res)=> {
+    console.log(req.params)
+    // convert the value pair in the obj to number
+    const id = req.params.id * 1;
+    const tour = tours.find(e => e.id === id);
+    if (!tour || id > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res
+        .status(200)
+        .json({
+            status: "Success",
+            data: {
+                tour
+            },
+    })
+})
+
 app.post('/api/v1/tours', (req, res)=> {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body)
+
+    tours.push(newTour);
+    
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+        res.status(201).json({
+            status: 'success!',
+            data: {
+                tour: newTour
+            }
+        })
+    })
     
 })
 
