d02923e7c1d575deae35faa62a8fe6a5b84712a3
Ayinde Ayobami <ayinde.ayobami@venturegardengroup.com>
2020-04-14 22:55:30 +0100

    Mounting routes

--- app.js
+++ app.js
@@ -106,34 +106,73 @@ const deleteTour = (req, res) => {
 	});
 }
 
+const getAllUsers = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+
+const getAllUsers = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+const getUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+const createUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+const updateUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+const deleteUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: "route is not yet defined"
+  })
+}
+
 // Routes
-app
-  .route('/api/v1/tours')
+// Mounting the Routers
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter
+  .route('/')
   .get(getAllTours)
   .post(createTour)
 
-app
-  .route('/api/v1/tours/:id')
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour)
 
-app
-  .route('/api/v1/users')
+userRouter
+  .route('/')
   .get(getAllUsers)
   .post(createUser)
 
-app
-  .route('/api/v1/users/:id')
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser)
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('api/v1/tours/:id', updateTour);
-// app.delete('api/v1/tours/:id', deleteTour);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 setTimeout(() => {
   throbber.stopAndPersist({
